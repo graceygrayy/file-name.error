a9e562ff877f2ef3c812107645c11c1a34f12b3a
Acheampong Grace <nanayaaaidoo1000@gmail.com>
2025-08-26 12:15:14 +0000

    Update week 4 session.py

--- week 4 session.py
+++ week 4 session.py
@@ -1,9 +1,9 @@
-file = open("grace_ contact.pdf", "w")
+file = open(",/WEEK4/grace_ contact.pdf", "w")
 file.write("grace1, grace2, grace3")
 
 # Reading content from a file
 
-file = open("grace_ contact.pdf", "r")
+file = open(",/WEEK4/grace_ contact.pdf", "r")
 content = file.read()
 print(content)
 
